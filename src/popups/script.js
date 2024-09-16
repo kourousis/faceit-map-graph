4039bc894b34f934e508e856ccdff9b5858a01a2
Ilias Kourousis <i.k.kourousis@gmail.com>
2024-09-16 22:39:18 +0100

    heptagons

---
+++ src/popups/script.js
@@ -0,0 +1,32 @@
+function createRegularPolygon(sides, radius) {
+    const angle = (Math.PI * 2) / sides;
+    let points = [];
+
+    for (let i = 0; i < sides; i++) {
+        const x = radius * Math.sin(i * angle);
+        const y = -radius * Math.cos(i * angle);
+        points.push(`${x.toFixed(2)}`, `${y.toFixed(2)}`);
+    }
+
+    return points.join(' ');
+}
+
+function createHeptagonElement(id, radius) {
+    const heptagon = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
+    heptagon.setAttribute('class', 'heptagon');
+    heptagon.setAttribute('id', id);
+    heptagon.setAttribute('points', createRegularPolygon(7, radius));
+    return heptagon;
+}
+
+const svgElement = document.querySelector('svg');
+const outermostRadius = 140;
+const innermostRadius = 20;
+const heptagonCount = 4;
+const radiusStep = (outermostRadius - innermostRadius) / (heptagonCount - 1);
+
+for (let i = 0; i < heptagonCount; i++) {
+    const radius = outermostRadius - (i * radiusStep);
+    const heptagonElement = createHeptagonElement(`heptagon-${i}`, radius);
+    svgElement.appendChild(heptagonElement);
+}
