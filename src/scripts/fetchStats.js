110d53d82eab435b71373e086695c62c1895a2d7
ilias <i.k.kourousis@gmail.com>
2024-09-24 12:04:03 +0100

    Move teamFriendlyMapData to getMatchMap()

--- src/scripts/fetchStats.js
+++ src/scripts/fetchStats.js
@@ -17,22 +17,24 @@ export const fetchStats = async () => {
         // Friendly matches
         let match_Ids = []
         match_Ids.push(await getFactionMatches(game, teamFriendly))
-        await fetchAllMatchMaps(match_Ids)
+        await getMatchMap(match_Ids)
 
         // Enemy matches
         // ...
-
+        console.log(teamFriendlyMapData, Object.keys(teamFriendlyMapData).length, "ENTRIES")
         //console.log(match_Ids)
-        console.log(teamFriendlyMapData, Object.keys(teamFriendlyMapData).length)
+        // console.log(teamFriendlyMapData, Object.keys(teamFriendlyMapData).length)
     } catch (error) {
         console.error('An error occurred:', error)
     }
 }
 
 const getFactionMatches = async (game, team, limit = 10) => {
+    let match_Ids_local = []
+
     for (let player of team) {
         const player_Id = await getPlayerId(player)
-        
+
         try {
             const response = await axios.get(`https://open.faceit.com/data/v4/players/${player_Id}/history`, {
                 headers: {
@@ -45,27 +47,17 @@ const getFactionMatches = async (game, team, limit = 10) => {
             });
 
             const matches_data = response.data.items
-            let match_Ids_local = []
 
             for (let match of matches_data) {
-                // let player_team = "faction2"
-                // for (let player of match.teams.faction1.players) {
-                //     if (player.player_id === player) {
-                //         player_team = "faction1"
-                //     }
-                // }
-
                 match_Ids_local.push(match.match_id)
-                // teamFriendlyMapData[match.match_id] = { player_team: player_team }
             }
-
-            return match_Ids_local
         } catch (error) {
             console.error('Error fetching matches:', error.response ? error.response.data : error.message)
             return []
         }
     }
 
+    return match_Ids_local
 }
 
 const getPlayerId = async (nickname) => {
@@ -85,42 +77,50 @@ const getPlayerId = async (nickname) => {
     }
 }
 
-const fetchAllMatchMaps = async (match_Ids) => {
+const getMatchMap = async (match_Ids) => {
+    const flat_match_Ids = match_Ids.flat();
+
     try {
-        const allMatchIds = match_Ids.flat();
-        await Promise.all(allMatchIds.map(single_match_id => getMatchMap(single_match_id)));
+        const promises = flat_match_Ids.map(match =>
+            axios.get(`https://open.faceit.com/data/v4/matches/${match}`, {
+                headers: {
+                    'Authorization': `Bearer ${api_key}`
+                }
+            }).then(response => {
+                let player_team = "faction2";
+                for (let player of response.data.teams.faction1.roster) {
+                    if (player.player_id === player) {
+                        player_team = "faction1";
+                    }
+                }
+
+                teamFriendlyMapData[match] = { player_team: player_team };
+
+                const winning_faction = response.data.results.winner;
+                const map = response.data.voting.map.pick[0];
+
+                if (teamFriendlyMapData[match] && teamFriendlyMapData[match].player_team) {
+                    if (teamFriendlyMapData[match].player_team == winning_faction) {
+                        // win
+                        teamFriendlyMapData[match].result = 1;
+                    } else {
+                        // loss
+                        teamFriendlyMapData[match].result = 0;
+                    }
+                } else {
+                    console.error(`teamFriendlyMapData for match ${match} is not properly initialized.`);
+                }
+
+                // Assign map
+                teamFriendlyMapData[match].map = map;
+            }).catch(error => {
+                console.error('Error fetching matches:', error.response ? error.response.data : error.message);
+            })
+        );
+
+        await Promise.all(promises);
+
     } catch (error) {
-        console.error('Error fetching all match maps:', error)
+        console.error('Error fetching matches:', error);
     }
-}
-
-const getMatchMap = async (match_id) => {
-    try {
-        const response = await axios.get(`https://open.faceit.com/data/v4/matches/${match_id}`, {
-            headers: {
-                'Authorization': `Bearer ${api_key}`
-            },
-        });
-
-        const winning_faction = response.data.results.winner;
-        const map = response.data.voting.map.pick[0]
-
-        if (teamFriendlyMapData[match_id] && teamFriendlyMapData[match_id].player_team) {
-            if (teamFriendlyMapData[match_id].player_team == winning_faction) {
-                // win
-                teamFriendlyMapData[match_id].result = 1
-            } else {
-                // loss
-                teamFriendlyMapData[match_id].result = 0
-            }
-        } else {
-            console.error(`teamFriendlyMapData for match_id ${match_id} is not properly initialized.`)
-        }
-
-        // Assign map
-        teamFriendlyMapData[match_id].map = map
-
-    } catch (error) {
-        console.error('Error fetching matches:', error.response ? error.response.data : error.message)
-    }
-}
+};
